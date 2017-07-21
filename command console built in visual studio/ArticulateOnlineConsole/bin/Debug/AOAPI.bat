5e458ca8eb40733e1490524f30b9dfe40e40f7f1
Brian Batt <bbatt@articulate.com>
2017-07-21 11:36:18 -0500

    Update AOAPI.bat
    Removed zappatest from this file

--- command console built in visual studio/ArticulateOnlineConsole/bin/Debug/AOAPI.bat
+++ command console built in visual studio/ArticulateOnlineConsole/bin/Debug/AOAPI.bat
@@ -1,1 +1,1 @@
-ArticulateOnlineConsole.exe https://zappatest.articulate-online.com DocumentList /EmailAddress=stevenzappa99@hotmail.com /Password=zappa99 /CustomerID=11178
+ArticulateOnlineConsole.exe https://yourdomain.articulate-online.com DocumentList /EmailAddress=aosuername /Password=YourPass /CustomerID=YourCustomID
